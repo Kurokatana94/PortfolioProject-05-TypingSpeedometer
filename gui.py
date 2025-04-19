4199e84062a1957ba721cdd358ad53951ace3e40
Kurokatana94 <70312018+Kurokatana94@users.noreply.github.com>
2025-04-19 02:12:35 +0100

    Add timer, and fixed multiple bugs

--- gui.py
+++ gui.py
@@ -4,7 +4,6 @@ from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 from logic import *
 from time import time
 
-
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
 
@@ -16,11 +15,27 @@ SOFT_WHITE = "#F3F3F3"
 SOFT_GREEN = "#52DF81"
 SOFT_PURPLE = "#C854FE"
 
+#TODO PLEASE THINK ABOUT THE REFRESH BUTTON
+#TODO YOU NEED TO ADD THE LOGIC AND FUNCTION (PROBABLY IN LOGIC.PY) FOR GETTING THE RESULTS, SAVING AND COMPARING ON THE LEADERBOARD
+
 def text_to_type_show():
+    #Refreshes the text to type area with a new text
+    text_to_type.config(state='normal')
     text_to_type.replace("1.0", 'end', ' '.join(refresh_words()))
+    text_to_type.config(state='disabled')
     text_to_type.tag_add("center", "1.0", "end")
-    timer_entry.delete(0, 'end')
-    timer_entry.insert(0, '60')
+    #Refreshes the timer to its default starting time
+    try:
+        timer_entry.config(state="normal")
+        timer_entry.delete(0, "end")
+        timer_entry.insert(0, "2")
+        timer_entry.config(state="disabled")
+    except Exception as e:
+        # timer_entry is not defined
+        print('The following error has occurred:', e)
+    #Refreshes the user input area to default
+    user_entry.delete(0, 'end')
+    reset_char_index()
     disable_button()
     # print(text_to_type.get(1.0, 'end'))
 
@@ -40,16 +55,76 @@ canvas = Canvas(
     relief="ridge"
 )
 canvas.place(x=0, y=0)
+# ----------------------------------------------------------------
+# Timer----------------------------------------------------
+# Used to keep track of the passed time
+old_time = None
 
+def timer_end():
+    global old_time
+    if int(timer_entry.get()) <= 0:
+        old_time = None
+        enable_button()
+
+def timer_update():
+    global old_time
+    if old_time is not None and time() - old_time > 1:
+        old_time = time()
+        time_left = int(timer_entry.get()) - 1
+        timer_entry.config(state="normal")
+        timer_entry.delete(0, 'end')
+        timer_entry.insert(0, str(time_left))
+        timer_entry.config(state="disabled")
+    timer_end()
+    window.after(10, timer_update)
+
+def timer_start():
+    global old_time
+    if old_time is None:
+        old_time = time()
+
+timer_bg = PhotoImage(
+    file=relative_to_assets("entry_3.png"))
+timer_entry_bg = canvas.create_image(
+    708.5,
+    370.75,
+    image=timer_bg
+)
+timer_entry = Entry(
+    font=("Arial BoldMT", 20 * -1),
+    bd=0,
+    bg=SOFT_WHITE,
+    fg=SOFT_RED,
+    disabledforeground=SOFT_RED,
+    highlightthickness=0,
+    justify='center'
+)
+timer_entry.place(
+    x=681.0,
+    y=353.5,
+    width=55.0,
+    height=32.5
+)
+#Sets the timer and stops the user to interact with it
+timer_entry.insert(0, '60')
+timer_entry.config(state="disabled")
+
+timer_im = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+timer = canvas.create_image(
+    652.0,
+    370.0,
+    image=timer_im
+)
 # ----------------------------------------------------------------
 # Start button ------------------------------------------
+#Disable
 def disable_button():
-    start_button.config(command=lambda: None)  # Disable logic
-    start_button.config(image=disable_btn_im)  # Keep original image
-    # Optional: add a tooltip or visual cue that it's "disabled"
+    start_button.config(command=lambda: None, image=disable_btn_im)
 
+#Enable
 def enable_button():
-    start_button.config(command=text_to_type_show)
+    start_button.config(command=lambda :text_to_type_show(), image=active_btn_im)
 
 disable_btn_im = PhotoImage(
     file=relative_to_assets("button_2.png"))
@@ -62,7 +137,7 @@ start_button = Button(
     borderwidth=0,
     highlightthickness=0,
     activebackground=SOFT_RED,
-    command=text_to_type_show,
+    command=lambda :text_to_type_show(),
     relief="flat",
 )
 start_button.place(
@@ -75,6 +150,37 @@ start_button.place(
 disable_button()
 
 # ---------------------------------------------------------------------------------
+# User entry area where they must type-----------------------------------------------
+user_box_im = PhotoImage(
+    file=relative_to_assets("entry_2.png"))
+user_entry_bg = canvas.create_image(
+    409.5,
+    370.0,
+    image=user_box_im)
+
+def validate(text):
+    if len(text) == 1 and text not in ACCEPTED_CHARS and text != ' ' and text != '-':
+        return False
+    return True
+
+user_entry = Entry(
+    validate='key',
+    validatecommand=(window.register(validate), "%S"),
+    font=("Arial BoldMT", 20 * -1),
+    bd=0,
+    bg=SOFT_WHITE,
+    fg="#000716",
+    highlightthickness=0,
+    justify='center',
+)
+user_entry.place(
+    x=259.0,
+    y=352.0,
+    width=301.0,
+    height=34.0
+)
+user_entry.bind('<Key>', lambda event: [check_correct_char(event, entry=user_entry, widget=text_to_type), timer_start()])
+# ----------------------------------------------------------------
 # Main text area where to show to user what to type--------------------------------
 text_to_type_im = PhotoImage(
     file=relative_to_assets("entry_1.png"))
@@ -105,91 +211,6 @@ text_to_type.tag_configure("correct", foreground=SOFT_GREEN)
 text_to_type.tag_configure("wrong", foreground=SOFT_PURPLE)
 text_to_type_show()
 text_to_type.config(state="disabled")
-# ----------------------------------------------------------------
-# User entry area where they must type-----------------------------------------------
-user_box_im = PhotoImage(
-    file=relative_to_assets("entry_2.png"))
-user_entry_bg = canvas.create_image(
-    409.5,
-    370.0,
-    image=user_box_im)
-
-def validate(text):
-    if len(text) == 1 and text not in ACCEPTED_CHARS and text != ' ':
-        return False
-    return True
-
-user_entry = Entry(
-    validate='key',
-    validatecommand=(window.register(validate), "%S"),
-    font=("Arial BoldMT", 20 * -1),
-    bd=0,
-    bg=SOFT_WHITE,
-    fg="#000716",
-    highlightthickness=0,
-    justify='center'
-)
-user_entry.place(
-    x=259.0,
-    y=352.0,
-    width=301.0,
-    height=34.0
-)
-user_entry.bind('<Key>', lambda event: [check_correct_char(event), timer_start()])
-# ----------------------------------------------------------------
-# Timer----------------------------------------------------
-# Used to keep track of the passed time
-old_time = None
-
-def timer_end():
-    global old_time
-    if int(timer_entry.get()) <= 0:
-        old_time = None
-        enable_button()
-
-def timer_update():
-    global old_time
-    if old_time is not None and time() - old_time > 1:
-        old_time = time()
-        time_left = int(timer_entry.get()) - 1
-        timer_entry.delete(0, 'end')
-        timer_entry.insert(0, str(time_left))
-    timer_end()
-
-def timer_start():
-    global old_time
-    if old_time is None:
-        old_time = time()
-
-timer_bg = PhotoImage(
-    file=relative_to_assets("entry_3.png"))
-timer_entry_bg = canvas.create_image(
-    708.5,
-    370.75,
-    image=timer_bg
-)
-timer_entry = Entry(
-    font=("Arial BoldMT", 20 * -1),
-    bd=0,
-    bg=SOFT_WHITE,
-    fg=SOFT_RED,
-    highlightthickness=0,
-    justify='center'
-)
-timer_entry.place(
-    x=681.0,
-    y=353.5,
-    width=55.0,
-    height=32.5
-)
-timer_entry.insert(0, '60')
-timer_im = PhotoImage(
-    file=relative_to_assets("image_1.png"))
-timer = canvas.create_image(
-    652.0,
-    370.0,
-    image=timer_im
-)
 # ----------------------------------------------------------------
 # Results------------------------------------------------------
 canvas.create_text(
--- logic.py
+++ logic.py
@@ -1,12 +1,7 @@
 import requests
 from random import shuffle
-import gui
 
-#TODO Implement timer system
-#TODO Make the timer start when pressing space or started to type
-
-# Getting an array of 1k words from an api in blocks of 200 for each length (3 to 7 letters)
-word_list: list = ' '.join([' '.join(requests.get(url=f'https://random-word-api.vercel.app/api?words=200&length={n}').json()) for n in range(3, 8)]).split()
+word_list: list = []
 lines_list: list = []
 typed_words: dict = {}
 last_typed_char_index: int = -1
@@ -14,16 +9,19 @@ current_word_index: int = 0
 # Used to check if the typed char should be checked or not
 ACCEPTED_CHARS = list('abcdefghijklmnopqrstuvwxyz')
 
+# Getting an array of 1k words from an api in blocks of 200 for each length (3 to 7 letters)
+def get_words():
+    return ' '.join([' '.join(requests.get(url=f'https://random-word-api.vercel.app/api?words=200&length={n}').json()) for n in range(3, 8)]).split()
+
 #Creates a new list and shows it
 def refresh_words():
     global word_list
-    word_list = ' '.join([' '.join(requests.get(url=f'https://random-word-api.vercel.app/api?words=200&length={n}').json()) for n in range(3, 8)]).split()
+    word_list = get_words()
     shuffle(word_list)
     return word_list
 
 # Scrolls down one line when at the 3rd line
-def scroll_down():
-    widget = gui.text_to_type
+def scroll_down(widget):
     char_index = f"1.{last_typed_char_index}"
     bbox = widget.bbox(char_index)
     if bbox:
@@ -32,8 +30,7 @@ def scroll_down():
             widget.yview_scroll(1, 'units')
 
 # Scroll up by one line when deleting the first char of a line, or more when deleting the space before that char
-def scroll_up():
-    widget = gui.text_to_type
+def scroll_up(widget):
     char_index = f"1.{last_typed_char_index}"
     bbox = widget.bbox(char_index)
     if bbox:
@@ -63,57 +60,59 @@ def scroll_up():
 def get_new_word_char_index():
     return len(' '.join(word_list[:current_word_index]))
 
-def remove_extra_chars():
-    entry = gui.user_entry.get()
-    if entry.endswith(' '):
-        gui.user_entry.delete(0, 'end')
+def remove_extra_chars(entry):
+    if entry.get().endswith(' '):
+        entry.delete(0, 'end')
+
+def reset_char_index():
+    global last_typed_char_index
+    last_typed_char_index = -1
 
 #---------------------------------------------------------------------USER INPUT SECTION---------------------------------------------------------------------------------
 #Well, it ended up being the central core of the code. It handles the user input, checking if it reflects the content in the text_to_type widget
 #Changes color using tags giving a visual output to the user
-def check_correct_char(event):
+def check_correct_char(event, entry, widget):
     global last_typed_char_index
     global current_word_index
     global typed_words
     char = event.char
-    remove_extra_chars()
-    entry = gui.user_entry.get()
+    remove_extra_chars(entry=entry)
 
     if event.keysym == 'BackSpace' and last_typed_char_index > -1:
 
         #This here gets back the previous word that was typed in case you backspaced on a space. Also useful if user hit space multiple times by mistake or something
-        if gui.text_to_type.get(1.0, 'end')[last_typed_char_index] == ' ':
+        if widget.get(1.0, 'end')[last_typed_char_index] == ' ':
             current_word_index -= 1
-            gui.user_entry.insert(0 ,typed_words[word_list[current_word_index]] + ' ') #Adding space at the end here so that the deleted chara will be the space and not the last char of the inserted word. It feels more natural
+            entry.insert(0 ,typed_words[word_list[current_word_index]] + ' ') #Adding space at the end here so that the deleted chara will be the space and not the last char of the inserted word. It feels more natural
             typed_len = len(typed_words[word_list[current_word_index]])
             word_len = len(word_list[current_word_index])
             last_typed_char_index -= word_len - typed_len if typed_len < word_len else 1
-            scroll_up()
+            scroll_up(widget=widget)
 
         # Checking for length of inputted word in user entry, to then synchronize the text to type response in case the typed word is longer than required when deleting. Avoiding to "clear" chars that are no being removed
         # TLDR No weird behaviour when backspace
-        if len(gui.user_entry.get()) <= len(word_list[current_word_index]):
-            [gui.text_to_type.tag_remove(tag, f"1.{last_typed_char_index}") for tag in gui.text_to_type.tag_names()]
+        if len(entry.get()) <= len(word_list[current_word_index]):
+            [widget.tag_remove(tag, f"1.{last_typed_char_index}") for tag in widget.tag_names()]
             last_typed_char_index -= 1
 
     elif event.keysym == 'space':
         #When space is typed it will move the index to the beginning of the next word. Saving the user-typed word in a dictionary where the key is the correct word, picked from the words list
-        typed_words[word_list[current_word_index]] = gui.user_entry.get().replace(' ', '')
+        typed_words[word_list[current_word_index]] = entry.get().replace(' ', '')
         current_word_index += 1
         last_typed_char_index = get_new_word_char_index()
-        gui.user_entry.delete(0, 'end')
-        scroll_down()
+        entry.delete(0, 'end')
+        scroll_down(widget=widget)
         *_, l = typed_words.items()
         print(typed_words, l)
 
     #Way too many ands but I need them not to nest. Anyway checks if user didn't input weird chars (not totally sure if I need those ands anymore after I added the validation in gui, but whatever for now)
-    elif char != '' and char in ACCEPTED_CHARS and len(entry) < len(word_list[current_word_index]) and len(char) == 1:
+    elif char != '' and char in ACCEPTED_CHARS and len(entry.get()) < len(word_list[current_word_index]) and len(char) == 1:
         last_typed_char_index += 1
-        if gui.text_to_type.get(f"1.{last_typed_char_index}") == char:
-            gui.text_to_type.tag_add("correct", f"1.{last_typed_char_index}")
+        if widget.get(f"1.{last_typed_char_index}") == char:
+            widget.tag_add("correct", f"1.{last_typed_char_index}")
         else:
-            gui.text_to_type.tag_add("wrong", f"1.{last_typed_char_index}")
-        scroll_down()
-    gui.text_to_type.tag_add("center", "1.0", "end")
+            widget.tag_add("wrong", f"1.{last_typed_char_index}")
+        scroll_down(widget=widget)
+    widget.tag_add("center", "1.0", "end")
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------TIMER HANDLING SECTION-----------------------------------------------------------------------------
